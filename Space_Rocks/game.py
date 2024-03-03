3a0685dc4978baf6dfbf32d92463df590130a99f
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2024-03-03 23:29:56 +0100

    Add asteroid objects to the screen around the ship

--- Space_Rocks/game.py
+++ Space_Rocks/game.py
@@ -1,6 +1,6 @@
 import pygame as pg
 from utils import load_a_sprite
-from models import SpaceShip
+from models import SpaceShip, Asteroid
 
 class SpaceRocks:
     def __init__(self):
@@ -12,6 +12,8 @@ class SpaceRocks:
         self.background = load_a_sprite("space", False)
 
         self.spaceship = SpaceShip((400, 300))
+
+        self.asteroids = [Asteroid(self.screen, self.spaceship.position) for _ in range(6)]
 
     def game_loop(self):
         while True:
@@ -34,11 +36,19 @@ class SpaceRocks:
         elif pressed_key[pg.K_UP]:
             self.spaceship.accelerate()
 
+    @property
+    def game_objects(self):
+        return [*self.asteroids, self.spaceship]
+
     def _game_logic(self):
-        self.spaceship.move(self.screen)
+        for obj in self.game_objects:
+            obj.move(self.screen)
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
-        self.spaceship.draw(self.screen)
+        
+        for obj in self.game_objects:
+            obj.draw(self.screen)
+        
         pg.display.flip()
         self.clock.tick(30)
