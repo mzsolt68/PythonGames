df9e15ce369f031f8a357e4991d3be052c280b81
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2024-03-04 14:39:39 +0100

    Add utility method to load a sound file

--- Space_Rocks/utils.py
+++ Space_Rocks/utils.py
@@ -1,5 +1,6 @@
 from pygame.image import load
 from pygame.math import Vector2
+from pygame.mixer import Sound
 from pygame import Surface
 from pathlib import Path
 
@@ -16,3 +17,7 @@ def wrap_position(surface: Surface, position: Vector2) -> Vector2:
     x, y = position
     w, h = surface.get_size()
     return Vector2(x % w, y % h)
+
+def load_sound(name: str) -> Sound:
+    file_name = Path(__file__).parent / Path("assets/sounds/" + name + ".wav")
+    return Sound(file_name)
