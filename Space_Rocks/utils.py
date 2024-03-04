02d48c43778603db7cb357d2737be20e7a7c366f
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2024-03-04 14:56:51 +0100

    Add utility function to display text message

--- Space_Rocks/utils.py
+++ Space_Rocks/utils.py
@@ -1,7 +1,7 @@
 from pygame.image import load
 from pygame.math import Vector2
 from pygame.mixer import Sound
-from pygame import Surface
+from pygame import Surface, Color, Font
 from pathlib import Path
 
 def load_a_sprite(name: str, with_alpha: bool = True) -> Surface:
@@ -21,3 +21,9 @@ def wrap_position(surface: Surface, position: Vector2) -> Vector2:
 def load_sound(name: str) -> Sound:
     file_name = Path(__file__).parent / Path("assets/sounds/" + name + ".wav")
     return Sound(file_name)
+
+def print_text(surface: Surface, text: str, font: Font, color: Color = Color("tomato")):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.center = Vector2(surface.get_size()) / 2
+    surface.blit(text_surface, text_rect)
