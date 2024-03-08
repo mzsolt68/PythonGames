1103d9a205013f1ad4dbef6d8b21dc7d4975711d
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2024-03-08 22:57:31 +0100

    Add some initialization code

--- Breakout/breakout.py
+++ Breakout/breakout.py
@@ -3,7 +3,22 @@ import pygame as pg
 
 class Breakout():
     def __init__(self):
-        pass
+        pg.init()
+        pg.display.set_caption("Breakout")
+        self.clock = pg.time.Clock()
+        self.font = pg.font.Font(None, 64)
+
+        self.screen = pg.display.set_mode((800, 600))
+
 
     def mainloop(self):
-        pass
+        while True:
+            self.handle_input()
+
+    def handle_input(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                quit()
+        pressed_key = pg.key.get_pressed()
+        if pressed_key[pg.K_ESCAPE] or pressed_key[pg.K_q]:
+            quit()
