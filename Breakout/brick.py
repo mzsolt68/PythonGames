7caa3719e1e2182a87cb11daddcd5adeec0a02c9
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2024-03-09 22:58:12 +0100

    Add new brick class

---
+++ Breakout/brick.py
@@ -0,0 +1,13 @@
+from gameobject import GameObject
+from utils import load_a_sprite
+from pygame import Surface
+from pygame.math import Vector2
+from gamedefs import SCREEN_WIDTH
+
+class Brick(GameObject):
+    def __init__(self, position: tuple, filename: str):
+        self.position = Vector2(position)
+        super().__init__(position, load_a_sprite(filename))
+
+    def draw(self, surface: Surface):
+        surface.blit(self.sprite, self.position)
