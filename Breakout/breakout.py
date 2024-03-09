9bac39de65195a8d67d7d94573a646fa8c7d5476
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2024-03-09 16:31:14 +0100

    Add paddle to the screen

--- Breakout/breakout.py
+++ Breakout/breakout.py
@@ -1,5 +1,6 @@
 
 import pygame as pg
+from paddle import Paddle
 
 class Breakout():
     def __init__(self):
@@ -9,11 +10,14 @@ class Breakout():
         self.font = pg.font.Font(None, 64)
 
         self.screen = pg.display.set_mode((800, 600))
+        self.paddle = Paddle((400, 575))
 
 
     def mainloop(self):
         while True:
             self.handle_input()
+            self.draw()
+            self.clock.tick(60)
 
     def handle_input(self):
         for event in pg.event.get():
@@ -22,3 +26,13 @@ class Breakout():
         pressed_key = pg.key.get_pressed()
         if pressed_key[pg.K_ESCAPE] or pressed_key[pg.K_q]:
             quit()
+        elif pressed_key[pg.K_LEFT]:
+            self.paddle.move(-5)
+        elif pressed_key[pg.K_RIGHT]:
+            self.paddle.move(5)
+    
+    def draw(self):
+        self.screen.fill((0, 0, 0))
+        self.paddle.draw(self.screen)
+        pg.display.flip()
+
